d999b299a597579921a2a620d4ad8f65acdefca0
harishb2006 <imharishba@gmail.com>
2025-02-06 10:47:33 +0530

    connectdb

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
-const express =require('express');
-const app=express();
-const port=3000;
+require('dotenv').config(); 
 
-app.get('/ping',(req,res)=>{
-    res.send('pong!');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const dotenv =require("dotenv");
+const connectDatabase = require('./database');
+
+app.get('/ping', (req, res) => {
+    res.send('Pong!');
 });
 
-app.listen(port, ()=>{
-    console.log(`server is running at http://localhost:${port}`)
+connectDatabase();
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
 });
