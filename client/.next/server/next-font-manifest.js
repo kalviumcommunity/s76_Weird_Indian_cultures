21266a01e40e088d5e7c00a1fb5b9f89dbfb3b44
harishb2006 <imharishba@gmail.com>
2025-11-11 12:05:12 +0530

    Ignore .next folder

--- client/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/harish/Documents/Important_project/cultura_connect/s76_Weird_Indian_cultures/client/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
